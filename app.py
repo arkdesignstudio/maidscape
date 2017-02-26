5d7044dc95445ec3ffae16654c8e1f8c7f6bc506
Kevin Wu <kevinwu127@gmail.com>
2017-02-25 23:05:23 -0800

    added flask files

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+  return render_template('index.html')
+
+@app.route('/form')
+def form():
+  return render_template('form.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/pricing')
+def pricing():
+  return render_template('pricing.html')
